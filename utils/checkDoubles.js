648d89d574ee6f7be2ddbd14e24e48df8e498fab
amsteradams <adam.k92@hotmail.fr>
2022-09-15 14:12:15 +0200

    modif checkDouble

--- utils/checkDoubles.js
+++ utils/checkDoubles.js
@@ -5,37 +5,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const testFolder = __dirname + '/../storage';
 
-export default function checkDoubles(artistName, mail){
-    const arr = [];
+export default function checkDoubles(mail){
     const storage = fs.readdirSync(testFolder);
     for (let i = 0; i < storage.length; i++) {
-        const tmpArr = fs.readdirSync(testFolder + '/' + storage[i])
-        if(tmpArr.length > 0){
-            for (let j = 0; j < tmpArr.length; j++) {
-                arr.push(tmpArr[j]);
-            }
-        }
-    }
-    for (let i = 0; i < arr.length; i++) {
-        if(parseCsv(arr[i], artistName, mail) === false){
+        if(parseCsv(storage[i], mail) === false){
             return false;
         }
     }
     return true;
 }
 
-    function parseCsv(fileTitle, artistName, mail){
-    const titleArr = fileTitle.split('_');
-    const folder = titleArr[0] + 's'
-
-    const data = fs.readFileSync(testFolder + '/' + folder + '/' + fileTitle, {encoding: 'utf-8'})
+    function parseCsv(fileTitle, mail){
+    const data = fs.readFileSync(testFolder + '/' + fileTitle, {encoding: 'utf-8'})
     const arr = data.split(',');
-    return check(arr, artistName, mail);
+    return check(arr, mail);
 }
 
-function check(arr, artistName, mail){
+function check(arr, mail){
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] === artistName || arr[i] === mail){
+        if(arr[i] === mail){
             return false;
         }
     }
--- utils/writer.js
+++ utils/writer.js
@@ -6,55 +6,33 @@ import checkDoubles from './checkDoubles.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export const writer = (json) => {
+  if(json.mail && checkDoubles(json.mail) === true){
 
-    switch (json.type) {
-      case "collector":
-          if(checkDoubles(json.artistName, json.mail)){
-            const name = "collector_" + Date.now() + ".csv"
-            fs.appendFile(
-                path.resolve(__dirname + "/../storage/collectors/" + name),
-                "firstName,lastName,mail,date\n" + `${json.firstName},${json.lastName},${json.mail},${Date.now()}`,
-                function (err) {
-                if (err) throw err;
-                console.log('Collector added !');
-             });
-          }else{
-            throw new Error('user already registered');
-          }
-          break
-      case "artist":
-        if(checkDoubles(json.artistName, json.mail)){
-            const name1 = "artist_" + Date.now() + ".csv"
-            fs.appendFile(
-                path.resolve(__dirname + "/../storage/artists/" + name1),
-                "firstName,lastName,artistName,book,mail,phone,date\n" + `${json.firstName},${json.lastName},${json.artistName},${json.book},${json.mail},${json.phone},${Date.now()}`,
-                function (err) {
-                if (err) throw err;
-                console.log('Artist added !');
-             });
-            }else{
-              throw new Error('user already registered');
-            }
-            break;
-    
-      case "agent":
-            if(checkDoubles(json.artistName, json.mail)){
-            const name2 = "agent_" + Date.now() + ".csv"
-            fs.appendFile(
-                path.resolve(__dirname + "/../storage/agents/" + name2),
-                "firstName,lastName,artistName,book,mail,phone,date\n" + `${json.firstName},${json.lastName},${json.artistName},${json.book},${json.mail},${json.phone},${Date.now()}`,
-                function (err) {
-                if (err) throw err;
-                console.log('Agent added !');
-             });
-            }else{
-              throw new Error('user already registered');
-            }
-            break;
-        
-        default:
-            throw new Error("unknown entry")
-    }
+    const name2 = "prospect_" + Date.now() + ".csv"
+  if(json.type === "agent" || json.type === "artist"){
+
+    fs.appendFile(
+      path.resolve(__dirname + "/../storage/" + name2),
+      "type,firstName,lastName,artistName,book,mail,phone,date\n" + `${json.type},${json.firstName},${json.lastName},${json.artistName},${json.book},${json.mail},${json.phone ? json.phone : "000"},${Date.now()}`,
+      function (err) {
+      if (err) throw err;
+      console.log('prospect added !');
+    })
+  }else if(json.type === "collector"){
+
+    fs.appendFile(
+      path.resolve(__dirname + "/../storage/" + name2),
+      "type,firstName,lastName,artistName,book,mail,phone,date\n" + `${json.type},${json.firstName ? json.firstName : "000"},${json.lastName ? json.lastName : "000"},"000","000",${json.mail},${json.phone ? json.phone : "000"},${Date.now()}`,
+      function (err) {
+      if (err) throw err;
+      console.log('prospect added !');
+    })
+  }else{
+
+    throw new Error("Bad request, need a valid json")
+  }
+  }else{
+    throw new Error('Mail already used')
+  }
 }
-
 export default writer
